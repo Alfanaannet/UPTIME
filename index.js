5c25f2a8e99cd928b34e70b5bdac7534f8fcb1dc
Alfanaannet <134984185+Alfanaannet@users.noreply.github.com>
2024-02-26 04:26:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const client = new Client({
   else file(client);
 });
 
-client.login(process.env.TOKEN).catch((error) => { console.log((error.message).bold.red) });
+client.login(set.TOKEN).catch((error) => { console.log((error.message).bold.red) });
 
 
 module.exports = client;
