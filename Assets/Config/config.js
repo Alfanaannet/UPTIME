e8a6dac8c0ead66132f539cb7fab3cd0d20df839
Alfanaannet <134984185+Alfanaannet@users.noreply.github.com>
2024-02-26 04:10:23 +0200

    Update config.js

--- Assets/Config/config.js
+++ Assets/Config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  TOKEN: process.env.TOKEN || "MTIxMTQ0NTU3NjczNTEzNzg5Mw.GFVX8H.K3zTydKlLJY-JlaAhvnIbA7N4lWmGVPgxdH5-Q",
-  MONGO_DB: process.env.MONGO_DB || "mongodb+srv://aossam:hossam@hossam.gzmbbcj.mongodb.net/?retryWrites=true&w=majority",
+  TOKEN: process.env.TOKEN || "",
+  MONGO_DB: process.env.MONGO_DB || "",
   CLIENT_ID: process.env.CLIENT_ID || "1211445576735137893",
   OWNERS: ["775012312876711936","907374921607430194"],
   SUPPORT_SERVER: "917580196251594815",
